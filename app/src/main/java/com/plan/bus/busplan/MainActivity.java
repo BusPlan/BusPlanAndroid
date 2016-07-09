7e421c13eeaab2093bd0b0ff73b29631a65abd75
slawek <kowalskislawomirkarol@gmail.com>
2016-07-09 10:49:39 +0200

    travis config

--- app/src/main/java/com/plan/bus/busplan/MainActivity.java
+++ app/src/main/java/com/plan/bus/busplan/MainActivity.java
@@ -1,7 +1,7 @@
 package com.plan.bus.busplan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
